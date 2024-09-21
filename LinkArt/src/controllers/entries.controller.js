3ba17e75c8b738799b26c1bdc28808143cf7f297
Rene Sandoval <sandovalrenemc@gmail.com>
2024-09-21 18:48:21 -0500

    Cambio de estructura

---
+++ LinkArt/src/controllers/entries.controller.js
@@ -0,0 +1,13 @@
+const renderIndex = (req, res) => {
+    res.render('index');
+};
+
+const renderNewEntry = (req, res) => {};
+
+const createNewEntry = (req, res) => {};
+
+module.exports = {
+    renderIndex,
+    renderNewEntry,
+    createNewEntry
+};
