49c3ae3c3cfc6aebdd031700e6346fba10785bc5
Rene Sandoval <sandovalrenemc@gmail.com>
2024-10-09 18:29:10 -0500

    Añadido el login, footer

--- LinkArt/src/controllers/entries.controller.js
+++ LinkArt/src/controllers/entries.controller.js
@@ -1,5 +1,21 @@
+/*
+Para añadir una funcion de renderizado de por ejemplo usuarios.ejs, copiaremos las lineas cambiando el nombre
+const renderUsuario = (req, res) => {
+    res.render('index');
+después añadimos "renderUsuario," en la parte de module.exports
+
+};
+
+*/
+
+
+
 const renderIndex = (req, res) => {
     res.render('index');
+};
+
+const renderLogin = (req, res) => {
+    res.render('login');
 };
 
 const renderNewEntry = (req, res) => {};
@@ -9,5 +25,6 @@ const createNewEntry = (req, res) => {};
 module.exports = {
     renderIndex,
     renderNewEntry,
-    createNewEntry
+    createNewEntry,
+    renderLogin
 };
