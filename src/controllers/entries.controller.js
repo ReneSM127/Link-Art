f641c0b12ab556b7626e7cefd1f833638d240eda
Rene Sandoval <sandovalrenemc@gmail.com>
2024-10-27 21:34:39 -0500

    Encriptar contraseña y envio de datos a la bd

--- src/controllers/entries.controller.js
+++ src/controllers/entries.controller.js
@@ -1,4 +1,5 @@
 const connection = require("../db")
+const bcrypt = require("bcryptjs");
 /*
 Para añadir una funcion de renderizado de por ejemplo usuarios.ejs, copiaremos las lineas cambiando el nombre
 const renderUsuario = (req, res) => {
@@ -30,17 +31,43 @@ const renderArtistas = (req, res) => {
 const renderNewEntry = (req, res) => {};
 
 const createUser = (req, res) => {
+    const q = "SELECT * FROM usuarios WHERE nombreUsuario = ?"; //Verifica si existe el usuario
+    connection.query(q, [req.body.username],(err, data) =>{
+        if (err){
+            console.error('Error insertando datos:', err);
+            return res.status(500).send('Error al insertar los datos en la base de datos.');
+        }
+
+        if (data.length) return res.status(409).send("User already exist");
+
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+        const query = "INSERT INTO usuarios (correo, nombreUsuario, nombre, apellidos, contrasena) VALUES (?)";
+
+        const VALUES = [
+            req.body.email,
+            req.body.username,
+            req.body.nombre,
+            req.body.apellido,
+            hashedPassword
+        ];
+        connection.query(query, [VALUES], (err, result) => {
+            if (err) {
+              console.error('Error insertando datos:', err);
+              return res.status(500).send('Error al insertar los datos en la base de datos.');
+            }
+        
+            // Redirigir o mostrar un mensaje de éxito
+            res.redirect("/");
+        });
+
+
+    })
+
     const correo = req.body.email;
     const query = 'INSERT INTO usuarios(correo) VALUES (?)';
-    connection.query(query, correo, (err, result) => {
-        if (err) {
-          console.error('Error insertando datos:', err);
-          return res.status(500).send('Error al insertar los datos en la base de datos.');
-        }
     
-        // Redirigir o mostrar un mensaje de éxito
-        res.redirect("/");
-    });
 
 };
 
