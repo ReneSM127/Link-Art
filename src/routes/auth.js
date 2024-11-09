ff0b1cd0607ea0e01589075d7d344f43f14cfe3e
Rene Sandoval <sandovalrenemc@gmail.com>
2024-11-08 20:02:10 -0500

    Inicio de sesion con bd y añadido de librerias

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 const router = Router();
 
 const {
-    createUser
+    createUser,
+    login
 } = require("../controllers/auth.js")
 
 router.post("/auth", createUser);
+router.post("/log", login);
 
 module.exports = router;
