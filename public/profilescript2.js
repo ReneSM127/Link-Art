1e9cc0da50dcfec61a67367c453fb23898638696
Rene Sandoval <sandovalrenemc@gmail.com>
2024-11-19 19:35:33 -0500

    modificacion y añadir compras

--- public/profilescript2.js
+++ public/profilescript2.js
@@ -1,25 +1,25 @@
 // Obtener el modal
-var modal = document.getElementById("modal");
+var modal2 = document.getElementById("modal2");
 
 // Obtener el botón que abre el modal
-var btn = document.querySelector(".add-post-btn");
+var btn2 = document.querySelector(".add-post-btn");
 
 // Obtener el elemento <span> que cierra el modal
-var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close2")[0];
 
 // Cuando el usuario hace clic en el botón, abre el modal
-btn.onclick = function() {
-    modal.style.display = "block";
+btn2.onclick = function() {
+    modal2.style.display = "flex";
 }
 
 // Cuando el usuario hace clic en <span> (x), cierra el modal
-span.onclick = function() {
-    modal.style.display = "none";
+span2.onclick = function() {
+    modal2.style.display = "none";
 }
 
 // Cuando el usuario hace clic fuera del modal, también cierra el modal
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 }
